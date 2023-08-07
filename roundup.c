de1210369843bc74660d00f97043a67b43889b4c
Luis Chamberlain <mcgrof@kernel.org>
2023-08-07 15:22:54 -0700

    roundup: add support for dynamic count
    This let's us put a cap on the loop. So for example the last_idx
    here shows it will always be outside of the scope of the operation.
    
    So to get the *inclusive* folio index we'd have to use last_idx - nrpages.
    Which is what we need on filemap_get_pages() when used with
    filemap_get_read_batch().
    
    ./roundup --order 2 --offset 16382 --offset-is-address --count 2
    Order: 2  nrpages: 4, Offset: 16382  Count: 2
    i:     0  pos: 16382  idx:     0  idx_next:     4  last_idx:     4
    i:     1  pos: 16383  idx:     0  idx_next:     4  last_idx:     4
    
    ./roundup --order 2 --offset 16382 --offset-is-address --count 4
    Order: 2  nrpages: 4, Offset: 16382  Count: 4
    i:     0  pos: 16382  idx:     0  idx_next:     4  last_idx:     8
    i:     1  pos: 16383  idx:     0  idx_next:     4  last_idx:     8
    i:     2  pos: 16384  idx:     4  idx_next:     8  last_idx:     8
    i:     3  pos: 16385  idx:     4  idx_next:     8  last_idx:     8
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- roundup.c
+++ roundup.c
@@ -24,7 +24,7 @@ static bool repeat_next_round_up = false;
 static bool offset_is_address = false;
 static unsigned int order = 2;
 static unsigned int offset = 0;
-unsigned int bytes = 50;
+unsigned int count = 50;
 
 static void usage(const char *cmd)
 {
@@ -33,6 +33,7 @@ static void usage(const char *cmd)
 	printf("--help                           this help menu\n");
 	printf("--order                          page order, default is 2 for 16k if on x86\n");
 	printf("--offset                         initial count offset, default is 0\n");
+	printf("--count                          how many times to loop, default is 50\n");
 	printf("--offset-is-address              the given offset should be treated as an address instead of an index\n");
 	printf("--repeat-idx-round-down          repeats round_down() on idx\n");
 	printf("--repeat-next-round-up           repeats round_up() on next\n");
@@ -67,6 +68,13 @@ void check_arg(const char *cmd, char *argv[], int *argc)
 		offset = atoi(argv[*argc]);
 		return;
 	}
+	if (strcmp(argv[*argc], "--count") == 0) {
+		*argc = (*argc) + 1;
+		if (*argc <= 1)
+			usage(cmd);
+		count = atoi(argv[*argc]);
+		return;
+	}
 	usage(cmd);
 }
 
@@ -81,9 +89,9 @@ int main(int argc, char *argv[])
 		check_arg(cmd_argv, argv, &i);
 
 	nrpages = 1UL << order;
-	printf("Order: %u  nrpages: %u, Offset: %u  Bytes: %u\n", order, nrpages, offset, bytes);
+	printf("Order: %u  nrpages: %u, Offset: %u  Count: %u\n", order, nrpages, offset, count);
 
-	for (i=0; i < bytes; i++) {
+	for (i=0; i < count; i++) {
 		unsigned int pos = offset + i;
 		unsigned int idx;
 		unsigned int idx_next;
@@ -98,7 +106,7 @@ int main(int argc, char *argv[])
 			idx = round_down(pos >> PAGE_SHIFT, nrpages);
 			idx_next = round_up(idx + 1, nrpages);
 			if (!last_idx_set) {
-				last_idx = DIV_ROUND_UP(pos + bytes, PAGE_SIZE);
+				last_idx = DIV_ROUND_UP(pos + count, PAGE_SIZE);
 				last_idx = round_up(last_idx, nrpages);
 				last_idx_set = true;
 			}
@@ -106,7 +114,7 @@ int main(int argc, char *argv[])
 			idx = round_down(pos, nrpages);
 			idx_next = round_up(pos, nrpages);
 			if (!last_idx_set) {
-				last_idx = DIV_ROUND_UP(pos + bytes, nrpages);
+				last_idx = DIV_ROUND_UP(pos + count, nrpages);
 				last_idx_set = true;
 			}
 		}
